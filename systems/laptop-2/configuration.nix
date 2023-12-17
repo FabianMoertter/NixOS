6037677b73a4e472836ba6e9fda50f1ba4cc08c3
Fabian Moertter <fabian.moertter@gmx.net>
2023-12-17 23:51:29 +0100

    remove users

--- systems/laptop-2/configuration.nix
+++ systems/laptop-2/configuration.nix
@@ -156,31 +156,6 @@
     ];
   };
 
-  # users.users.eva = {
-  #   isNormalUser = true;
-  #   description = "Eva";
-  #   extraGroups = [ "networkmanager" ];
-  #   packages = with pkgs; [
-  #     discord
-  #     firefox
-  #     google-chrome
-  #     libreoffice
-  #     skypeforlinux
-  #   ];
-  # };
-  #
-  # users.users.guest = {
-  #   isNormalUser = true;
-  #   description = "Guest";
-  #   extraGroups = [ "networkmanager" ];
-  #   packages = with pkgs; [
-  #     discord
-  #     firefox
-  #     google-chrome
-  #     skypeforlinux
-  #   ];
-  # };
-
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
