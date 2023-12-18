36ae7876a2dab94f5e06e2fede5373241cded4b3
Fabian Moertter <fabian.moertter@gmx.net>
2023-12-18 01:28:05 +0100

    add neorg

--- modules/home-manager/nvim/init.lua
+++ modules/home-manager/nvim/init.lua
@@ -207,6 +207,12 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+
+  {
+    -- Neorg
+    'nvim-neorg/neorg',
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
