15bf0c8bdbe84cec6fda51527cb954e0c18e654f
Fabian Moertter <fabian.moertter@gmx.net>
2023-12-22 14:23:49 +0100

    update neovim

--- modules/home-manager/nvim/init.lua
+++ modules/home-manager/nvim/init.lua
@@ -298,21 +298,6 @@ require 'custom.keymaps'
 -- [[ Utils ]]
 -- require 'custom.utils'
 
--- save with CTRL+s
-vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', { silent = true, desc = 'save file' })
-
--- navigate within insert mode
-vim.keymap.set('i', '<C-h>', '<Left>', { silent = true, desc = 'move left in insert mode' })
-vim.keymap.set('i', '<C-l>', '<Right>', { silent = true, desc = 'move right in insert mode' })
-vim.keymap.set('i', '<C-j>', '<Down>', { silent = true, desc = 'move down in insert mode' })
-vim.keymap.set('i', '<C-k>', '<Up>', { silent = true, desc = 'move up in insert mode' })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
