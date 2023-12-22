58e8a43a0002b5cab793789b676df15cf7b07525
Fabian Moertter <fabian.moertter@gmx.net>
2023-12-22 15:54:50 +0100

    update neovim

--- modules/home-manager/nvim/lua/plugins/telescope.lua
+++ modules/home-manager/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,16 @@
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
 return {
   "nvim-telescope/telescope.nvim"
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-u>'] = false,
+            ['<C-d>'] = false,
+          },
+        },
+      },
+    })
 }
