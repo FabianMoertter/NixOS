414f4f7d62d0a972a16efb16ab8e4524b17c85a7
Fabian Moertter <fabian.moertter@gmx.net>
2024-01-28 18:29:48 +0100

    update

--- systems/desktop/configuration.nix
+++ systems/desktop/configuration.nix
@@ -142,6 +142,7 @@ in
     # add keys here
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGS4kNhWQxe9+DtJofug5UDdYtp6JEfsc0tySQ+WvuNa fabian@hymenoptera"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAX6riUhWVDNUyteX4S4sm7JOEsBiMmajfiZ+RHvDLfq fabian.moertter@gmx.net"
     ];
   };
   programs.zsh.enable = true; # maybe necessary if zsh default shell
