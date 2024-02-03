1e99878b8a0e2442aa17262cbb231e625f194d22
Fabian Moertter <fabian.moertter@gmx.net>
2024-02-03 13:42:11 +0100

    update

--- modules/system/homeServer.nix
+++ modules/system/homeServer.nix
@@ -1,7 +1,33 @@
 { config, lib, pkgs, ... }:
 {
-  # Cronjobs
+
+  # # Cronjobs
+  # services.cron = {
+  #   enable = true;
+  #   systemCronJobs = [
+  #     "*/5 * * * *    root    data >> /tmp/cron.log"
+  #   ];
+  # };
 
   # Services
+
+  # # Grafana
+  # services.grafane = {
+  #   enable = true;
+  #   settings = {
+  #     server = {
+  #       # Listening Address
+  #       http_addr = "127.0.0.1";
+  #       # Port
+  #       http_port = 3000;
+  #       # Grafana needs to know on which domain and URL it's running
+  #       domain = "your.domain";
+  #       root_url = "https://your.domain/grafana"; # Not needed if its is https://your.domain/
+  #       serve_from_sub_path = true;
+  #     };
+  #   };
+  # };
+
+  # # Nginx
 
 }
