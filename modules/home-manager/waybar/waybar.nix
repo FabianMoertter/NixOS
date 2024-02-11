47198666d1d8810c3665fdc6a6d82c2441dd22f1
Fabian Moertter <fabian.moertter@gmx.net>
2024-02-11 11:49:07 +0100

    update

--- modules/home-manager/waybar/waybar.nix
+++ modules/home-manager/waybar/waybar.nix
@@ -8,7 +8,7 @@
 
       modules-left = [ "clock" ];
       modules-center = [ ];
-      modules-right = [ "blueooth" ];
+      modules-right = [ "pulseaudio" "blueooth" ];
 
       "clock" = {
         format = "{: %I:%M %p}";
@@ -18,16 +18,36 @@
       "disk" = { };
 
       "bluetooth" = {
-        "format" = " {status}";
-        "format-disabled" = " {status}";
-        "format-connected" = " {num_connections}";
-        "tooltip-format" = "{device_alias}";
-        "format-connected-battery" = " {device_alias} {device_battery_percentage}%";
-        "tooltip-format-enumerate-connected" = "{device_alias}";
-        "format-off" = "";
-        "interval" = 30;
-        "on-click" = "blueman-manager";
+        format = " {status}";
+        format-disabled = " {status}";
+        format-connected = " {num_connections}";
+        tooltip-format = "{device_alias}";
+        format-connected-battery = " {device_alias} {device_battery_percentage}%";
+        tooltip-format-enumerate-connected = "{device_alias}";
+        format-off = "";
+        interval = 30;
+        on-click = "blueman-manager";
       };
+
+      "pulseaudio" = {
+        format = "{icon} {volume}% {format_source}";
+        format-bluetooth = "{volume}% {icon} {format_source}";
+        format-bluetooth-muted = " {icon} {format_source}";
+        format-muted = " {format_source}";
+        format-source = " {volume}%";
+        format-source-muted = "";
+        format-icons = {
+          headphone = "";
+          hands-free = "";
+          headset = "";
+          phone = "";
+          portable = "";
+          car = "";
+          default = [ "" "" "" ];
+        };
+        on-click = "pavucontrol";
+      };
+
 
     }];
   };
