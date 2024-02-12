4674e8de6e302b456c070e14aa32491a85ae3a74
Fabian Moertter <fabian.moertter@gmx.net>
2024-02-12 09:40:02 +0100

    update

--- modules/system/homeServer.nix
+++ modules/system/homeServer.nix
@@ -1,12 +1,6 @@
 { config, lib, pkgs, ... }:
 {
 
-  # Dashy
-  #services.dashy = {
-  #enable = true;
-  #port = 8081;
-  #};
-
   # # Cronjobs
   # services.cron = {
   #   enable = true;
@@ -30,6 +24,47 @@
     };
   };
 
-  # # Nginx
+  # Prometheus
+  services.prometheus = {
+    enable = true;
+    port = 8028;
+  };
 
+  # Loki
+
+  # PiHole
+
+  # Nginx
+
+  # Calibre-Web
+  services.calibre-web = {
+    enable = true;
+    user = "fabian";
+    listen = {
+      port = 8025;
+      ip = "192.168.0.252";
+    };
+    options = {
+      enableBookUploading = true;
+    };
+  };
+
+  # Paperless
+  services.paperless = {
+    enable = true;
+    # mediaDir = "";
+    # dataDir = "";
+    user = "fabian";
+    port = 8026;
+    address = "192.168.0.252";
+    passwordFile = "/home/fabian/home-server/paperless/paperless_auth.txt";
+  };
+
+  # Dashy
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 8501 8024 8025 8026 8027 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 }
--- systems/mini/configuration.nix
+++ systems/mini/configuration.nix
@@ -76,35 +76,6 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Calibre-Web
-  services.calibre-web = {
-    enable = true;
-    user = "fabian";
-    listen = {
-      port = 8025;
-      ip = "192.168.0.252";
-    };
-    options = {
-      enableBookUploading = true;
-    };
-  };
-
-  # Paperless
-  services.paperless = {
-    enable = true;
-    # mediaDir = "";
-    # dataDir = "";
-    user = "fabian";
-    port = 8026;
-    address = "192.168.0.252";
-    passwordFile = "/home/fabian/home-server/paperless/paperless_auth.txt";
-  };
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 8501 8024 8025 8026 8027 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
