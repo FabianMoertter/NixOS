341f949531ba58d37b261386d0791968f6134fcd
Fabian Moertter <fabian.moertter@gmx.net>
2024-02-12 08:52:56 +0100

    update

--- modules/system/homeServer.nix
+++ modules/system/homeServer.nix
@@ -3,8 +3,8 @@
 
   # Dashy
   #services.dashy = {
-    #enable = true;
-    #port = 8081;
+  #enable = true;
+  #port = 8081;
   #};
 
   # # Cronjobs
@@ -17,22 +17,18 @@
 
   # Services
 
-  # # Grafana
-  # services.grafane = {
-  #   enable = true;
-  #   settings = {
-  #     server = {
-  #       # Listening Address
-  #       http_addr = "127.0.0.1";
-  #       # Port
-  #       http_port = 3000;
-  #       # Grafana needs to know on which domain and URL it's running
-  #       domain = "your.domain";
-  #       root_url = "https://your.domain/grafana"; # Not needed if its is https://your.domain/
-  #       serve_from_sub_path = true;
-  #     };
-  #   };
-  # };
+  # Grafana
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "192.168.0.252";
+        http_port = 8027;
+        domain = "fabian.home";
+        serve_from_sub_path = true;
+      };
+    };
+  };
 
   # # Nginx
 
