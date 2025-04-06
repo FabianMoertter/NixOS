d67c0d5e08ca99ef5b9945ca88d224b87aabbe78
Fabian Moertter <fabian.moertter@gmx.net>
2025-04-06 14:13:35 +0200

    update

--- modules/system/homeServer.nix
+++ modules/system/homeServer.nix
@@ -67,6 +67,57 @@ in
     customCSS = "";
   };
 
+  # Traefik
+
+  services.traefik = {
+    enable = true;
+
+    staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = ":80";
+          asDefault = true;
+          http.redirections.entrypoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+
+        websecure = {
+          address = ":443";
+          asDefault = true;
+          # http.tls.certResolver = "letsencrypt";
+        };
+      };
+
+      # log = {
+      #   level = "INFO";
+      #   filePath = "${config.services.traefik.dataDir}/traefik.log";
+      #   format = "json";
+      # };
+
+      # certificatesResolvers.letsencrypt.acme = {
+      #   email = "postmaster@YOUR.DOMAIN";
+      #   storage = "${config.services.traefik.dataDir}/acme.json";
+      #   httpChallenge.entryPoint = "web";
+      # };
+
+      api.dashboard = true;
+      # Access the Traefik dashboard on <Traefik IP>:8080 of your server
+      # api.insecure = true;
+    };
+
+    # providers = {
+    #   file = "/etc/traefik/dynamic.toml";
+    #   watch = true;
+    # };
+
+    dynamicConfigOptions = {
+      http.routers = { };
+      http.services = { };
+    };
+  };
+
   # Grafana
   services.grafana = {
     enable = true;
