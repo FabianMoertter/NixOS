411f9cc20fcdc4b0f874239d0b41fd3509f5fd13
Fabian Moertter <fabian.moertter@gmx.net>
2025-04-15 21:19:19 +0200

    update

--- modules/system/homeServer.nix
+++ modules/system/homeServer.nix
@@ -80,7 +80,20 @@ in
       }
     ];
 
-    widgets = [ ];
+    widgets = [ {
+      resources = {
+        cpu = true;
+        disk = "/";
+        memory = true;
+      };
+    }
+    {
+      search = {
+        provider = "duckduckgo";
+        target = "_blank";
+      };
+    }
+    ];
 
     kubernetes = { };
 
@@ -88,58 +101,6 @@ in
 
     customJS = "";
     customCSS = "";
-  };
-
-  # Traefik
-
-  services.traefik = {
-    enable = false;
-
-    staticConfigOptions = {
-      entryPoints = {
-        web = {
-          address = ":80";
-          asDefault = true;
-          http.redirections.entrypoint = {
-            to = "websecure";
-            scheme = "https";
-          };
-        };
-
-        websecure = {
-          address = ":443";
-          asDefault = true;
-          # http.tls.certResolver = "letsencrypt";
-        };
-      };
-
-      # log = {
-      #   level = "INFO";
-      #   filePath = "${config.services.traefik.dataDir}/traefik.log";
-      #   format = "json";
-      # };
-
-      # certificatesResolvers.letsencrypt.acme = {
-      #   email = "postmaster@YOUR.DOMAIN";
-      #   storage = "${config.services.traefik.dataDir}/acme.json";
-      #   httpChallenge.entryPoint = "web";
-      # };
-
-      api.dashboard = true;
-      # Access the Traefik dashboard on <Traefik IP>:8080 of your server
-      # api.insecure = true;
-    };
-
-    # providers = {
-    #   file = "/etc/traefik/dynamic.toml";
-    #   watch = true;
-    # };
-
-    # dynamicConfigOptions = {
-    #   http.routers = { };
-    #   http.services = { };
-    # };
-
   };
 
   # Grafana
