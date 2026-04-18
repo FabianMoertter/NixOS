d12cfaca3a9a638e0a5804b0c662de86ac7ee696
Fabian Moertter <fabian.moertter@gmx.net>
2026-04-18 13:11:31 +0200

    cleanup

--- pkgs/neovim-switch.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellScriptBin "test-script" ''
-  echo "hello world" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
-''
--- shells/myshell.nix
+++
@@ -1,9 +0,0 @@
-# https://ghedam.at/15978/an-introduction-to-nix-shell
-with (import <nixpkgs> {});
-mkShell {
-  buildInputs = [
-  ];
-  shellHook = ''
-    echo "Hello"
-  '';
-}
