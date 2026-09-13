e753c5bff5cc0fcf08fe56d9695c13f0f37d1f6a
Fabian Moertter <fabian.moertter@gmx.net>
2026-09-13 17:54:20 +0200

    update

--- modules/home-manager/cli.nix
+++ modules/home-manager/cli.nix
@@ -191,18 +191,4 @@ in
     ];
   };
 
-  programs.vim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [ vim-airline vimwiki vim-tmux-navigator ];
-    settings = {
-      number = true;
-      relativenumber = true;
-      tabstop = 2;
-    };
-    extraConfig = ''
-      set mouse=a
-      let mapleader = " "
-      let g:vimwiki_list = [ { 'path': '$HOME/Projects/Neovim/vimwiki' } ]
-    '';
-  };
 }
